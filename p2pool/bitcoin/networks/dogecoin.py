96f8ad2814e6f6fff3ec7ef9bc03df76236437b5
CohibAA <CohibAA@users.noreply.github.com>
2014-11-02 18:33:31 -0700

    Update dogecoin.py

--- p2pool/bitcoin/networks/dogecoin.py
+++ p2pool/bitcoin/networks/dogecoin.py
@@ -7,22 +7,22 @@ from .. import data, helper
 from p2pool.util import pack
 
 
-P2P_PREFIX = 'c0c0c0c0'.decode('hex')
-P2P_PORT = 22556
-ADDRESS_VERSION = 30
-RPC_PORT = 22555
+P2P_PREFIX = 'fbc0b6db'.decode('hex')
+P2P_PORT = 9333
+ADDRESS_VERSION = 48
+RPC_PORT = 9332
 RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            'dogecoinaddress' in (yield bitcoind.rpc_help()) and
+            'litecoinaddress' in (yield bitcoind.rpc_help()) and
             not (yield bitcoind.rpc_getinfo())['testnet']
         ))
-SUBSIDY_FUNC = lambda height: 500000*100000000 >> (height + 1)//100000 #not bullet proof bot will be ok for now
+SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//840000
 POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
-BLOCK_PERIOD = 60 # s
-SYMBOL = 'DOGE'
-CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'DogeCoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Dogecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.dogecoin'), 'dogecoin.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'http://dogechain.info/block/'
-ADDRESS_EXPLORER_URL_PREFIX = 'http://dogechain.info/address/'
-TX_EXPLORER_URL_PREFIX = 'http://dogechain.info/tx/'
+BLOCK_PERIOD = 150 # s
+SYMBOL = 'LTC'
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Litecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Litecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.litecoin'), 'litecoin.conf')
+BLOCK_EXPLORER_URL_PREFIX = 'http://explorer.litecoin.net/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'http://explorer.litecoin.net/address/'
+TX_EXPLORER_URL_PREFIX = 'http://explorer.litecoin.net/tx/'
 SANE_TARGET_RANGE = (2**256//1000000000 - 1, 2**256//1000 - 1)
 DUMB_SCRYPT_DIFF = 2**16
 DUST_THRESHOLD = 0.03e8
